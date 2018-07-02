69dc998557edc321b55fd0ed572a53a583eb6349
MoranGoz <38177429+MoranGoz@users.noreply.github.com>
2018-07-02 16:54:05 +0300

    Revert "incUsersId"

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Order } from '../Order'
-import { Observable } from 'rxjs';
-import { OrdersService } from '../orders.service'
-import { MatTableDataSource } from '@angular/material';
-import { MotoBoy } from '../MotoBoy';
-import { MotoService } from '../moto.service';
-import {Customer} from '../customer'
-import { CustomerService } from '../customer.service';
-import { stringify } from '@angular/compiler/src/util';
 import { UsersService } from '../users.service';
-
 
 @Component({
   selector: 'app-profile',
@@ -17,11 +7,12 @@ import { UsersService } from '../users.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  currentUser = new MotoBoy ;
+
+  currentUser;
+
   rating: number = 3;
   starCount: number = 0;
   color: string = 'yellow';
-
 
   constructor(private userService: UsersService) { }
 
